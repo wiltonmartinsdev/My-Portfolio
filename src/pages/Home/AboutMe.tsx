48f845bf3ff3d9b3414e74effa4a1d6c39ea81bc
Wilton Martins <wiltonliramartins@icloud.com>
2024-08-13 22:29:16 -0300

    Style: The footer of the home-mobile page has been completed

--- src/pages/Home/AboutMe.tsx
+++ src/pages/Home/AboutMe.tsx
@@ -14,145 +14,187 @@ function AboutMe() {
 	const [isOpen, setIsOpen] = useState(false);
 
 	return (
-		<div className="min-w-80 pt-4 px-3 sm:flex">
-			<aside
-				className={`flex flex-col items-center justify-center mx-auto bg-neutral-800 overflow-hidden transition-[height] duration-1000 ease-in-out ${
-					isOpen ? "h-96" : "h-44"
-				} rounded-2xl mb-3`}>
-				<span className=" w-full flex justify-end">
-					<Hamburger
-						toggled={isOpen}
-						toggle={setIsOpen}
-						size={26}
-						color="white"
-						rounded
-					/>
-				</span>
-				<div className="flex items-center gap-3 mb-10">
-					<img
-						className="border border-neutral-600  w-20 bg-transparent rounded-2xl hover:border-2 hover:border-orange-600"
-						src={profileImage}
-						alt="Foto do perfil"
-					/>
+		<>
+			<div className="min-w-80 pt-4 px-3 pb-20 sm:flex">
+				<aside
+					className={`flex flex-col items-center justify-center mx-auto bg-neutral-800 overflow-hidden transition-[height] duration-1000 ease-in-out ${
+						isOpen ? "h-96" : "h-44"
+					} rounded-2xl mb-3`}>
+					<span className=" w-full flex justify-end">
+						<Hamburger
+							toggled={isOpen}
+							toggle={setIsOpen}
+							size={26}
+							color="white"
+							rounded
+						/>
+					</span>
+					<div className="flex items-center gap-3 mb-10">
+						<img
+							className="border border-neutral-600  w-20 bg-transparent rounded-2xl hover:border-2 hover:border-orange-600"
+							src={profileImage}
+							alt="Foto do perfil"
+						/>
 
-					<div>
-						<p className="text-white text-center text-lg hover:text-orange-600">
-							Wilton Lira Martins
-						</p>
+						<div>
+							<p className="text-white text-center text-lg hover:text-orange-600">
+								Wilton Lira Martins
+							</p>
 
-						<Badge>Desenvolvedor Front-end</Badge>
+							<Badge>Desenvolvedor Front-end</Badge>
+						</div>
 					</div>
-				</div>
 
-				{isOpen && (
-					<div className=" w-72 text-white text-center border-t border-orange-600 mb-3 pt-5 space-y-3 px-2">
-						<div className=" flex items-center gap-2">
-							<DeviceMobile
-								size={24}
-								color="#ea580c"
-							/>
-							<div className="text-left text-xs">
-								<a
-									className="hover:text-orange-600"
-									href="tel:+55 91 98250-4940">
-									(91) 98250-4940
-								</a>
+					{isOpen && (
+						<div className=" w-72 text-white text-center border-t border-orange-600 mb-3 pt-5 space-y-3 px-2">
+							<div className=" flex items-center gap-2">
+								<DeviceMobile
+									size={24}
+									color="#ea580c"
+								/>
+								<div className="text-left text-xs">
+									<a
+										className="hover:text-orange-600"
+										href="tel:+55 91 98250-4940">
+										(91) 98250-4940
+									</a>
+								</div>
 							</div>
-						</div>
 
-						<div className=" flex items-center gap-2">
-							<EnvelopeSimple
-								size={24}
-								color="#ea580c"
-							/>
-							<div className="text-left text-xs">
-								<a
-									className="hover:text-orange-600"
-									href="mailto:wiltonliramartins@icloud.com">
-									wiltonliramartins@icloud.com
-								</a>
+							<div className=" flex items-center gap-2">
+								<EnvelopeSimple
+									size={24}
+									color="#ea580c"
+								/>
+								<div className="text-left text-xs">
+									<a
+										className="hover:text-orange-600"
+										href="mailto:wiltonliramartins@icloud.com">
+										wiltonliramartins@icloud.com
+									</a>
+								</div>
 							</div>
-						</div>
 
-						<div className=" flex items-center gap-2">
-							<GithubLogo
-								size={24}
-								color="#ea580c"
-							/>
-							<div className="text-left text-xs">
-								<a
-									className="hover:text-orange-600"
-									href="https://github.com/wiltonmartinsdev"
-									target="_blank">
-									GitHub
-								</a>
+							<div className=" flex items-center gap-2">
+								<GithubLogo
+									size={24}
+									color="#ea580c"
+								/>
+								<div className="text-left text-xs">
+									<a
+										className="hover:text-orange-600"
+										href="https://github.com/wiltonmartinsdev"
+										target="_blank">
+										GitHub
+									</a>
+								</div>
 							</div>
-						</div>
 
-						<div className=" flex items-center gap-2">
-							<LinkedinLogo
-								size={24}
-								color="#ea580c"
-							/>
-							<div className="text-left text-xs">
-								<a
-									className="hover:text-orange-600"
-									href="https://www.linkedin.com/in/wiltonliramartins"
-									target="_blank">
-									LinkedIn
-								</a>
+							<div className=" flex items-center gap-2">
+								<LinkedinLogo
+									size={24}
+									color="#ea580c"
+								/>
+								<div className="text-left text-xs">
+									<a
+										className="hover:text-orange-600"
+										href="https://www.linkedin.com/in/wiltonliramartins"
+										target="_blank">
+										LinkedIn
+									</a>
+								</div>
 							</div>
+
+							<div className="loader"></div>
 						</div>
+					)}
+				</aside>
 
-						<div className="loader"></div>
-					</div>
-				)}
-			</aside>
+				<main className="h-auto p-5 rounded-2xl bg-neutral-800 sm:flex sm:flex-1">
+					<h1 className="text-orange-600 font-semibold mb-4 text-3xl">
+						Sobre mim
+					</h1>
 
-			<main className="h-auto p-5 rounded-2xl bg-neutral-800 sm:flex sm:flex-1">
-				<h1 className="text-orange-600 font-semibold mb-4 text-3xl">
-					Sobre mim
-				</h1>
+					<p className="text-white mb-4">
+						Olá, bem-vindo (a), ao meu portfólio!
+					</p>
 
-				<p className="text-white mb-4">
-					Olá, bem-vindo (a), ao meu portfólio!
-				</p>
+					<ul className="text-white">
+						<li className="mb-4">
+							Sou um entusiasta da programação em busca de
+							oportunidades como Programador Júnior, meu foco é
+							utilizar minhas habilidades técnicas em {""}
+							<span className="text-orange-600">
+								HTML, CSS, JavaScript e React.js
+							</span>{" "}
+							para impactar positivamente a vida das pessoas
+							através da tecnologia.
+						</li>
+						<li className="mb-4">
+							Programação para mim não é o que eu quero hoje, é o
+							que eu quero para minha vida! Embora eu esteja no
+							começo da minha trajetória, me vejo diariamente como
+							programador, as dificuldades impostas no caminho são
+							grandes, mas, o que eu aprendi na programação é que
+							nunca devemos desistir e sempre persistir! A
+							constância dos meus estudos me levará aos meus
+							objetivos.
+						</li>
+						<li className="mb-4">
+							Desde meu primeiro contato, de fato, com a
+							programação em 2019, minha paixão por essa área
+							cresceu exponencialmente. Atualmente, estou
+							aprimorando meus conhecimentos na Rocketseat, onde
+							tenho a oportunidade de me desenvolver
+							constantemente.
+						</li>
+						<li>
+							Estou entusiasmado para contribuir com projetos
+							desafiadores e aprender com profissionais
+							experientes. Se você está em busca de um Programador
+							Jr. dedicado, apaixonado por programação e pronto
+							para agregar valor à sua equipe, estou à disposição
+							para conectar e colaborar.
+						</li>
+					</ul>
+				</main>
+			</div>
 
-				<ul className="text-white">
-					<li className="mb-4">
-						Sou um entusiasta da programação em busca de
-						oportunidades como Programador Júnior, meu foco é
-						utilizar minhas habilidades técnicas em HTML, CSS,
-						JavaScript e React.js para impactar positivamente a vida
-						das pessoas através da tecnologia.
-					</li>
-					<li className="mb-4">
-						Programação para mim não é o que eu quero hoje, é o que
-						eu quero para minha vida! Embora eu esteja no começo da
-						minha trajetória, me vejo diariamente como programador,
-						as dificuldades impostas no caminho são grandes, mas, o
-						que eu aprendi na programação é que nunca devemos
-						desistir e sempre persistir! A constância dos meus
-						estudos me levará aos meus objetivos.
-					</li>
-					<li className="mb-4">
-						Desde meu primeiro contato, de fato, com a programação
-						em 2019, minha paixão por essa área cresceu
-						exponencialmente. Atualmente, estou aprimorando meus
-						conhecimentos na Rocketseat, onde tenho a oportunidade
-						de me desenvolver constantemente.
-					</li>
-					<li>
-						Estou entusiasmado para contribuir com projetos
-						desafiadores e aprender com profissionais experientes.
-						Se você está em busca de um Programador Jr. dedicado,
-						apaixonado por programação e pronto para agregar valor à
-						sua equipe, estou à disposição para conectar e
-						colaborar.
-					</li>
-				</ul>
-			</main>
-		</div>
+			<footer className="bg-neutral-800 h-14 flex justify-center items-center rounded-t-2xl fixed bottom-0 left-0 right-0 z-10 sm:hidden">
+				<nav className="">
+					<ul className="text-white flex gap-3">
+						<li>
+							<a
+								className="hover:text-orange-600"
+								href="#">
+								Sobre mim
+							</a>
+						</li>
+						<li>
+							<a
+								className="hover:text-orange-600"
+								href="#">
+								Resumo
+							</a>
+						</li>
+						<li>
+							<a
+								className="hover:text-orange-600"
+								href="#">
+								Projetos
+							</a>
+						</li>
+						<li>
+							<a
+								className="hover:text-orange-600"
+								href="#">
+								Contato
+							</a>
+						</li>
+					</ul>
+				</nav>
+			</footer>
+		</>
 	);
 }
 
