e1c0107bfec57f33639e6acdf6008d67fa3ebd96
Wilton Martins <wiltonliramartins@icloud.com>
2024-08-15 16:03:38 -0300

    Style: Aside, desktop version finished

---
+++ src/components/Aside/Aside.tsx
@@ -0,0 +1,128 @@
+import { Divide as Hamburger } from "hamburger-react";
+import { useState, useEffect } from "react";
+
+import { Badge } from "@/components/ui/badge";
+import {
+	DeviceMobile,
+	EnvelopeSimple,
+	GithubLogo,
+	LinkedinLogo,
+} from "@phosphor-icons/react";
+import profileImage from "@/assets/perfil.png";
+
+function Aside() {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const [isLargeScreen, setIsLargeScreen] = useState(
+		window.innerWidth >= 1024
+	);
+
+	// Atualiza o estado quando o tamanho da tela mudar
+	useEffect(() => {
+		const handleResize = () => {
+			setIsLargeScreen(window.innerWidth >= 1024);
+		};
+
+		window.addEventListener("resize", handleResize);
+
+		// Cleanup do event listener quando o componente for desmontado
+		return () => window.removeEventListener("resize", handleResize);
+	}, []);
+
+	return (
+		<aside
+			className={`border border-neutral-700 flex flex-col items-center justify-center mx-auto bg-neutral-800 overflow-hidden transition-[height] duration-1000 ease-in-out ${
+				isOpen || isLargeScreen ? "h-96 lg:h-[560px]" : "h-44"
+			} rounded-2xl mb-3 lg:w-72 lg:mx-0 lg:sticky lg:top-16`}>
+			<span className="w-full flex justify-end lg:hidden">
+				<Hamburger
+					toggled={isOpen}
+					toggle={setIsOpen}
+					size={26}
+					color="white"
+					rounded
+				/>
+			</span>
+
+			<div className="flex items-center gap-3 mb-10 lg:flex lg:flex-col">
+				<img
+					className="border border-neutral-600 w-20 bg-transparent rounded-2xl hover:border-2 hover:border-orange-600 lg:w-32"
+					src={profileImage}
+					alt="Foto do perfil"
+				/>
+
+				<div>
+					<p className="text-white text-center text-lg hover:text-orange-600">
+						Wilton Lira Martins
+					</p>
+
+					<Badge>Desenvolvedor Front-end</Badge>
+				</div>
+			</div>
+
+			{(isOpen || isLargeScreen) && (
+				<div className="w-72 text-white text-center border-t border-orange-600 mb-3 pt-5 space-y-3 px-2 lg:pl-5 lg:w-60">
+					<div className="flex items-center gap-2">
+						<DeviceMobile
+							size={24}
+							color="#ea580c"
+						/>
+						<div className="text-left text-xs">
+							<a
+								className="hover:text-orange-600"
+								href="tel:+55 91 98250-4940">
+								(91) 98250-4940
+							</a>
+						</div>
+					</div>
+
+					<div className="flex items-center gap-2">
+						<EnvelopeSimple
+							size={24}
+							color="#ea580c"
+						/>
+						<div className="text-left text-xs">
+							<a
+								className="hover:text-orange-600"
+								href="mailto:wiltonliramartins@icloud.com">
+								wiltonliramartins@icloud.com
+							</a>
+						</div>
+					</div>
+
+					<div className="flex items-center gap-2">
+						<GithubLogo
+							size={24}
+							color="#ea580c"
+						/>
+						<div className="text-left text-xs">
+							<a
+								className="hover:text-orange-600"
+								href="https://github.com/wiltonmartinsdev"
+								target="_blank">
+								GitHub
+							</a>
+						</div>
+					</div>
+
+					<div className="flex items-center gap-2">
+						<LinkedinLogo
+							size={24}
+							color="#ea580c"
+						/>
+						<div className="text-left text-xs">
+							<a
+								className="hover:text-orange-600"
+								href="https://www.linkedin.com/in/wiltonliramartins"
+								target="_blank">
+								LinkedIn
+							</a>
+						</div>
+					</div>
+				</div>
+			)}
+		</aside>
+	);
+}
+
+export default Aside;
--- src/pages/Home/AboutMe.tsx
+++ src/pages/Home/AboutMe.tsx
@@ -1,123 +1,21 @@
 import {
-	DeviceMobile,
-	EnvelopeSimple,
-	GithubLogo,
-	LinkedinLogo,
-} from "@phosphor-icons/react";
-import {
 	Card,
 	CardDescription,
 	CardHeader,
 	CardTitle,
 } from "@/components/ui/card";
-import { Divide as Hamburger } from "hamburger-react";
 import { Laptop } from "lucide-react";
-import { useState } from "react";
 
-import { Badge } from "@/components/ui/badge";
-import profileImage from "@/assets/perfil.png";
+import { DeviceMobile } from "@phosphor-icons/react";
+import Aside from "@/components/Aside/Aside";
 
 function AboutMe() {
-	const [isOpen, setIsOpen] = useState(false);
-
 	return (
 		<>
-			<div className="min-w-80 pt-4 px-3 xsm:px-14 pb-20 lg:flex lg:gap-6">
-				<aside
-					className={`border border-neutral-700 flex flex-col items-center justify-center mx-auto bg-neutral-800 overflow-hidden transition-[height] duration-1000 ease-in-out ${
-						isOpen ? "h-96" : "h-44"
-					} rounded-2xl mb-3`}>
-					<span className=" w-full flex justify-end">
-						<Hamburger
-							toggled={isOpen}
-							toggle={setIsOpen}
-							size={26}
-							color="white"
-							rounded
-						/>
-					</span>
-					<div className="flex items-center gap-3 mb-10">
-						<img
-							className="border border-neutral-600  w-20 bg-transparent rounded-2xl hover:border-2 hover:border-orange-600"
-							src={profileImage}
-							alt="Foto do perfil"
-						/>
+			<div className="min-w-80 pt-16 px-3 xsm:px-14 pb-20 lg:flex lg:justify-center lg:gap-6 lg:w-full">
+				<Aside />
 
-						<div>
-							<p className="text-white text-center text-lg hover:text-orange-600">
-								Wilton Lira Martins
-							</p>
-
-							<Badge>Desenvolvedor Front-end</Badge>
-						</div>
-					</div>
-
-					{isOpen && (
-						<div className="w-72 text-white text-center border-t border-orange-600 mb-3 pt-5 space-y-3 px-2 lg:hidden">
-							<div className=" flex items-center gap-2">
-								<DeviceMobile
-									size={24}
-									color="#ea580c"
-								/>
-								<div className="text-left text-xs">
-									<a
-										className="hover:text-orange-600"
-										href="tel:+55 91 98250-4940">
-										(91) 98250-4940
-									</a>
-								</div>
-							</div>
-
-							<div className=" flex items-center gap-2">
-								<EnvelopeSimple
-									size={24}
-									color="#ea580c"
-								/>
-								<div className="text-left text-xs">
-									<a
-										className="hover:text-orange-600"
-										href="mailto:wiltonliramartins@icloud.com">
-										wiltonliramartins@icloud.com
-									</a>
-								</div>
-							</div>
-
-							<div className=" flex items-center gap-2">
-								<GithubLogo
-									size={24}
-									color="#ea580c"
-								/>
-								<div className="text-left text-xs">
-									<a
-										className="hover:text-orange-600"
-										href="https://github.com/wiltonmartinsdev"
-										target="_blank">
-										GitHub
-									</a>
-								</div>
-							</div>
-
-							<div className=" flex items-center gap-2">
-								<LinkedinLogo
-									size={24}
-									color="#ea580c"
-								/>
-								<div className="text-left text-xs">
-									<a
-										className="hover:text-orange-600"
-										href="https://www.linkedin.com/in/wiltonliramartins"
-										target="_blank">
-										LinkedIn
-									</a>
-								</div>
-							</div>
-
-							<div className="loader"></div>
-						</div>
-					)}
-				</aside>
-
-				<main className="border border-neutral-700 min-h-screen p-5 rounded-2xl bg-neutral-800 lg:flex lg:flex-1">
+				<main className="border border-neutral-700  p-5 rounded-2xl bg-neutral-800 lg:flex lg:flex-col lg:w-[900px]">
 					<h1 className="text-orange-600 font-semibold mb-4 text-3xl">
 						Sobre mim
 					</h1>
@@ -132,7 +30,7 @@ function AboutMe() {
 							oportunidades como Programador Júnior, meu foco é
 							utilizar minhas habilidades técnicas em {""}
 							<span className="text-orange-600">
-								HTML, CSS, JavaScript e React.js
+								HTML, CSS, JavaScript e React
 							</span>{" "}
 							para impactar positivamente a vida das pessoas
 							através da tecnologia.
