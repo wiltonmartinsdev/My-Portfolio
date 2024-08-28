a729cc680f9c9968b0a46a5a679a862766e139b3
Wilton Martins <wiltonliramartins@icloud.com>
2024-08-28 14:50:21 -0300

    Style: Finished the responsiveness of the cards, on the Projects page

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -17,7 +17,7 @@ const SelectTrigger = React.forwardRef<
   <SelectPrimitive.Trigger
     ref={ref}
     className={cn(
-      "flex h-10 w-full items-center justify-between rounded-md border border-neutral-600 bg-neural-700 px-3 py-2 text-sm ring-offset-orange-600 placeholder:text-slate-500 focus:outline-none focus:ring-1 focus:ring-orange-600 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1 dark:border-slate-800 dark:bg-slate-950 dark:ring-offset-slate-950 dark:placeholder:text-slate-400 dark:focus:ring-slate-300",
+      "flex mb-8 h-10 w-full items-center justify-between rounded-md border border-neutral-600 bg-neural-700 px-3 py-2 text-sm ring-offset-orange-600 placeholder:text-slate-500 focus:outline-none focus:ring-1 focus:ring-orange-600 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1 dark:border-slate-800 dark:bg-slate-950 dark:ring-offset-slate-950 dark:placeholder:text-slate-400 dark:focus:ring-slate-300",
       className
     )}
     {...props}
