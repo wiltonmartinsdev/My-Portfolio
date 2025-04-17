32a829b26534cd62acc204094442c0b746611709
Wilton Martins <wiltonliramartins@icloud.com>
2025-04-17 11:43:23 -0300

    docs(Footer): update developer attribution with emoji
    Update the footer text to include a heart emoji for a more engaging developer attribution

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ export function Footer() {
         <footer className="border border-neutral-700 bg-neutral-800 h-14 flex justify-center items-center rounded-t-2xl fixed bottom-0 left-0 right-0 z-10">
 				<div className="flex flex-col items-center justify-center gap-0.5">
 					<p className=" text-white hover:text-orange-600 text-xs cursor-default">
-						Desenvolvido por Wilton Lira Martins
+                    Desenvolvido com ❤️ pelo dev: Wilton Lira Martins
 					</p>
 
 					<p className="text-white hover:text-orange-600 text-xs cursor-default">
